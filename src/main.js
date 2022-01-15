3830202b78cff2a9762c7d06909a332d4b568869
sn7996618@gmail.com <97530341+sangdz0711@users.noreply.github.com>
2022-01-15 11:16:28 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import Navigo from "navigo";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import AboutPage from "./pages/about";
+import HomePage from "./pages/home";
+import NewsDetail from "./pages/newsDetail";
+
+const router = new Navigo("/", { linksSelector: "a" });
+
+const print = (content) => {
+    document.getElementById("header").innerHTML = Header.render();
+    document.getElementById("content").innerHTML = content;
+    document.getElementById("footer").innerHTML = Footer.render();
+};
+
+router.on({
+    "/": () => {
+        print(HomePage.render());
+    },
+    "/about": () => {
+        print(AboutPage.render());
+    },
+    "/news/:id": ({ data }) => {
+        const { id } = data;
+        print(NewsDetail.render(id));
+    },
+    "/home": () => {
+        print(AboutPage.render());
+    },
+});
+
+router.resolve();
